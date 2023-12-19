51607604ad38c00441b55bbc90fda90d5f094de7
bobozhengsir <bobozhengsir@gmail.com>
2023-12-19 16:34:15 +0800

    bench: add simple bench (#2)

--- example/go/impl.go
+++ example/go/impl.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 type Demo struct{}
 
 func init() {
@@ -9,15 +7,12 @@ func init() {
 }
 
 func (Demo) demo_oneway(req DemoUser) {
-	fmt.Printf("[oneway] Golang received name: %s, age: %d\n", req.name, req.age)
 }
 
 func (Demo) demo_check(req DemoComplicatedRequest) DemoResponse {
-	fmt.Printf("[call] Golang received req\n")
 	return DemoResponse{pass: true}
 }
 
 func (Demo) demo_check_async(req DemoComplicatedRequest) DemoResponse {
-	fmt.Printf("[call async] Golang received req\n")
 	return DemoResponse{pass: true}
 }

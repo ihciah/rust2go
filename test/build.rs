48ccc2f8db7366894acf278cc2de7d03fa8a17fc
ihciah <ihciah@gmail.com>
2023-12-26 14:45:40 +0000

    test: add intergrated test

---
+++ test/build.rs
@@ -0,0 +1,3 @@
+fn main() {
+    rust2go::Builder::new().with_go_src("./go").build();
+}
---
+++ test/src/user.rs
@@ -0,0 +1,66 @@
+pub mod binding {
+    #![allow(warnings)]
+    rust2go::r2g_include_binding!();
+}
+
+#[derive(rust2go::R2G, Clone)]
+pub struct User {
+    pub id: u32,
+    pub name: String,
+    pub age: u8,
+}
+
+#[derive(rust2go::R2G, Clone)]
+pub struct LoginRequest {
+    pub user: User,
+    pub password: String,
+}
+
+#[derive(rust2go::R2G, Clone)]
+pub struct LoginResponse {
+    pub succ: bool,
+    pub message: String,
+    pub token: Vec<u8>,
+}
+
+#[derive(rust2go::R2G, Clone)]
+pub struct LogoutRequest {
+    pub token: Vec<u8>,
+    pub user_ids: Vec<u32>,
+}
+
+#[derive(rust2go::R2G, Clone)]
+pub struct FriendsListRequest {
+    pub token: Vec<u8>,
+    pub user_ids: Vec<u32>,
+}
+
+#[derive(rust2go::R2G, Clone)]
+pub struct FriendsListResponse {
+    pub users: Vec<User>,
+}
+
+#[derive(rust2go::R2G, Clone)]
+pub struct PMFriendRequest {
+    pub user_id: u32,
+    pub token: Vec<u8>,
+    pub message: String,
+}
+
+#[derive(rust2go::R2G, Clone)]
+pub struct PMFriendResponse {
+    pub succ: bool,
+    pub message: String,
+}
+
+#[rust2go::r2g]
+pub trait TestCall {
+    fn ping(n: usize) -> usize;
+    fn login(req: &LoginRequest) -> LoginResponse;
+    fn logout(req: &User);
+    async fn add_friends(req: &FriendsListRequest) -> FriendsListResponse;
+    #[drop_safe]
+    async fn delete_friends(req: FriendsListRequest) -> FriendsListResponse;
+    #[drop_safe_ret]
+    async fn pm_friend(req: PMFriendRequest) -> PMFriendResponse;
+}
